bb790ae38eb4211b4f9c32051c0734afa6767e51
1499maxawergy <91216092+1499maxawergy@users.noreply.github.com>
2022-02-27 11:16:10 +0300

    Pract 15
    Added Pract 15

---
+++ Pract_15/src/main/java/ru/max/Pract_15/SQLBaseConfig.java
@@ -0,0 +1,7 @@
+package ru.max.Pract_15;
+
+public class SQLBaseConfig {
+    public static final String DB_USERNAME="root";
+    public static final String DB_PASSWORD="123";
+    public static final String DB_URL="jdbc:postgresql://localhost:5432/groups";
+}
---
+++ Pract_15/src/test/java/ru/max/Pract_15/Pract15ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.max.Pract_15;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pract15ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
