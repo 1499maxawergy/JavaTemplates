f61288b07faf0422d0d1440398d884eb10a90e05
1499maxawergy <91216092+1499maxawergy@users.noreply.github.com>
2022-03-01 17:20:59 +0300

    Pract 19
    Added Pract 19

---
+++ Pract_19/src/main/java/ru/max/Pract_16/SpringBootApplication.java
@@ -0,0 +1,12 @@
+package ru.max.Pract_16;
+
+import org.springframework.boot.SpringApplication;
+
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootApplication.class, args);
+	}
+
+}
